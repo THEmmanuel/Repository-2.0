b1d52373abf7346960ec9fd3db283690f6bf6aaf
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-07-20 03:10:42 +0100

    Styled the introduction div and CTA buttons

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -5,32 +5,35 @@ import SecondaryCTA from '../../Components/Buttons/SecondaryCTA/SecondaryCTA';
 
 const Home = (props) => {
     return (
-        <div className={style.Introduction}>
-            <div>
-                <span className = {style.NamePre}>
-                    Hi there, I'm
+        <div className={style.Home}>
+            <div className={style.Introduction}>
+                <div>
+
+                    <span className={style.NamePre}>
+                        Hi there, I'm
                 </span>
 
-                <span className = {style.Name}>
-                    Emmanuel.
-            </span>
+                    <span className={style.Name}>
+                        Emmanuel.
+                </span>
 
-                <span className = {style.ShortDescription}>
-                    I craft Amazing web experiences.
-            </span>
+                    <span className={style.ShortDescription}>
+                        I craft Amazing web experiences.
+                </span>
 
-                <span className = {style.LongDescription}>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa, suscipit accusamus ea eaque eos veniam ipsam quam deserunt laborum quibusdam unde modi optio dolorem ad a adipisci, distinctio est totam!
-            </span>
+                    <span className={style.LongDescription}>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa, suscipit accusamus ea eaque eos veniam ipsam quam deserunt laborum quibusdam unde modi optio dolorem ad a adipisci, distinctio est totam!
+                </span>
 
-            </div>
-            <div className={style.Buttons}>
-                <PrimaryCTA />
-                <SecondaryCTA />
+                </div>
+                <div className={style.Buttons}>
+                    <PrimaryCTA text='See My Work' />
+                    <SecondaryCTA text='About Me' />
+                </div>
+
             </div>
 
         </div>
-
     )
 }
 
