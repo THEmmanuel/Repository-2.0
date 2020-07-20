ea7614936cb04836232594ec85812844308b05a3
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-07-20 03:50:43 +0100

    Added the NavBar component

---
+++ src/Containers/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <div>
+            Logo will be here
+        </div>
+
+        <div>
+            
+        </div>
+    )
+}
+
+export default NavBar;
