76db82a9bdab23eab8e785bcca70c9525becf0e8
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-07-21 21:36:54 +0100

    Added Portfolio animation

--- src/Containers/Portfolio/Portfolio.js
+++ src/Containers/Portfolio/Portfolio.js
@@ -2,19 +2,34 @@ import React from 'react';
 import style from './Portfolio.module.css';
 import Project from '../../Components/Cards/Project/Project';
 import TestImage from '../../Assets/testImage.jpg'
+import PortfolioAnimation from '../../Assets/SVG/Asset 8.svg';
 
 const Portfolio = () => {
     return (
 
         <div className={style.Portfolio}>
-            <div className = {style.Projects}>
+            <div className={style.Projects}>
                 <Project
-                    ProjectImage = {TestImage}
-                    
+                    ProjectImage={TestImage}
+                />
+
+                <Project
+                    ProjectImage={TestImage}
+                />
+
+                <Project
+                    ProjectImage={TestImage}
+                />
+
+                <Project
+                    ProjectImage={TestImage}
                 />
             </div>
+
+            <img src={PortfolioAnimation} className={style.PortfolioAnimation} alt="" />
+
         </div>
-    
+
     )
 }
 
