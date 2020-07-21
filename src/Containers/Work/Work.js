c314835d1ef9a32f949e7875407d8e28883d0485
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-07-21 21:49:03 +0100

    Added Work component and style module

---
+++ src/Containers/Work/Work.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './Work.module.css';
+
+
+const Work = () => {
+    return (
+        <div className={style.Work}>
+            
+        </div>
+    )
+}
+
+export default Work;
