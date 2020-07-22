c1df8f5a296474123d1c2c252487e69828629bf3
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-07-22 10:28:08 +0100

    Added WorkAnimation SVG to work component

--- src/Containers/Work/Work.js
+++ src/Containers/Work/Work.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import style from './Work.module.css';
+import WorkAnimation from '../../Assets/SVG/Asset 7.svg'
 
 
 const Work = () => {
     return (
         <div className={style.Work}>
-            
+            <div className = {style.WorkExperiences}>
+                <span>
+                    Work Experiences will appear here
+                </span>
+            </div>
+            <img src={WorkAnimation} alt="" className = {style.WorkAnimation}/>
         </div>
     )
 }
