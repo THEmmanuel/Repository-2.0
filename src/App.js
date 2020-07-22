8abe88bc041d7204893c8c73c942413b2cf5f5f8
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-07-22 10:36:53 +0100

    Added About imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import NavBar from './Containers/NavBar/NavBar'
+// import NavBar from './Containers/NavBar/NavBar'
 import Home from './Containers/Home/Home';
 import Portfolio from './Containers/Portfolio/Portfolio';
 import Work from './Containers/Work/Work';
-
+import About from './Containers/About/About'
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
         <Home/>
         <Portfolio/>
         <Work/>
+        <About/>
     </div>
   );
 }
