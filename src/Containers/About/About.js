5c7802b004ea1364c4df8688d4042a9e422320a6
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-07-23 01:31:15 +0100

    Added Dummy content to the about section

--- src/Containers/About/About.js
+++ src/Containers/About/About.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import style from './About.module.css'
+import TestImage from '../../Assets/testImage.jpg'
 
 const About = () => {
     return (
-        <div className = {style.About}>
-            About will appear here
-        </div>
+        <section className = {style.About}>
+            <div className = {style.AboutContent}>
+                <img src={TestImage} alt="" className = {style.AboutImage}/>
+                <div>
+                    <span className = {style.AboutDescription}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde odio tenetur quisquam natus fugit earum fuga qui quibusdam quod? Officiis sequi minima nobis quos, sint tempora mollitia repudiandae aut recusandae.
+
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nulla repellendus vel veritatis velit error, qui magnam asperiores, voluptates, quibusdam repudiandae! Dolor nulla hic illo velit quas? Sed, eum earum?
+                    </span>
+                </div>
+            </div>
+        </section>
     )
 }
 
