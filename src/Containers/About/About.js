2205023d90ab6acd38af8e9ed99bd1070fa3a64c
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-07-23 14:05:46 +0100

    Added Styling to the About component

--- src/Containers/About/About.js
+++ src/Containers/About/About.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import style from './About.module.css'
 import TestImage from '../../Assets/testImage.jpg'
+import AboutAnimation from '../../Assets/SVG/Asset 9.svg'
 
 const About = () => {
     return (
@@ -10,11 +11,12 @@ const About = () => {
                 <div>
                     <span className = {style.AboutDescription}>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde odio tenetur quisquam natus fugit earum fuga qui quibusdam quod? Officiis sequi minima nobis quos, sint tempora mollitia repudiandae aut recusandae.
-
+                        <p/>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nulla repellendus vel veritatis velit error, qui magnam asperiores, voluptates, quibusdam repudiandae! Dolor nulla hic illo velit quas? Sed, eum earum?
                     </span>
                 </div>
             </div>
+            <img src={AboutAnimation} alt="" className = {style.AboutAnimation}/>
         </section>
     )
 }
