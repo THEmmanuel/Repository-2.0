e9f1881ff8ada41cf5b8ccbe62cf4d99337d7a97
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-07-24 06:52:36 +0100

    Added basic styling to the Contact container

--- src/Containers/Contact/Contact.js
+++ src/Containers/Contact/Contact.js
@@ -5,8 +5,8 @@ import ContactButton from '../../Components/Buttons/ContactButton/ContactButton'
 const Contact = (props) => {
     return (
         <section className={style.Contact}>
-            <div>
-                <span>
+            <div className = {style.ContactContent}>
+                <span className={style.ContactText}>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae facere repellat sint possimus cum iure illo aspernatur atque eos ipsam odio repudiandae inventore, eius aperiam esse blanditiis consectetur, ex dignissimos.
                 </span>
 
