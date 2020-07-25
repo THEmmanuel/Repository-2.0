0048d26fa90bcd0bbe5e14ffa0fa2f653b10dd0d
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-07-25 03:30:55 +0100

    Added general improvements

--- src/Containers/Work/Work.js
+++ src/Containers/Work/Work.js
@@ -6,12 +6,19 @@ import WorkAnimation from '../../Assets/SVG/Asset 7.svg'
 const Work = () => {
     return (
         <div className={style.Work}>
-            <div className = {style.WorkExperiences}>
+            <div className={style.WorkExperiences}>
                 <span>
                     Work Experiences will appear here
                 </span>
+
+                <div className = {style.WorkExperience}>
+                    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique tenetur magni aliquid! Recusandae quidem itaque dolorem facere magnam quas sit mollitia ut ad reprehenderit a sapiente obcaecati, suscipit accusamus nulla.</span>
+
+                    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id facere accusantium earum dicta voluptatibus inventore architecto quaerat tempore, quisquam fugit, omnis soluta dolore explicabo voluptatum illum magni qui. Accusantium, commodi.</span>
+                </div>
+
             </div>
-            <img src={WorkAnimation} alt="" className = {style.WorkAnimation}/>
+            <img src={WorkAnimation} alt="" className={style.WorkAnimation} />
         </div>
     )
 }
