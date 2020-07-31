07281a35ea7af467e62dd3dcc5fe624a65a5a6fd
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-07-31 03:45:10 +0100

    Added work TimeLine indicator div and styling

--- src/Containers/Work/Work.js
+++ src/Containers/Work/Work.js
@@ -7,12 +7,18 @@ const Work = () => {
     return (
         <div className={style.Work}>
             <div className={style.WorkExperiences}>
-            <h2 className = {style.SectionTitle}>
-                Work Experience.
+                <h2 className={style.SectionTitle}>
+                    Work Experience.
             </h2>
 
-                <div className = {style.WorkExperience}>
+                <div className={style.WorkExperience}>
                     <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique tenetur magni aliquid! Recusandae quidem itaque dolorem facere magnam quas sit mollitia ut ad reprehenderit a sapiente obcaecati, suscipit accusamus nulla.</span>
+
+                    <div className={style.WorkTimelineIndicator}>
+                        <div className={style.TimeIndicator}></div>
+                        <span className={style.line} ></span>
+                        <div className={style.TimeIndicator}></div>
+                    </div>
 
                     <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id facere accusantium earum dicta voluptatibus inventore architecto quaerat tempore, quisquam fugit, omnis soluta dolore explicabo voluptatum illum magni qui. Accusantium, commodi.</span>
                 </div>
