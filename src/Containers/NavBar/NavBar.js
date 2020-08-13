eb767fb918c02ed7cb8605c54d10bd170b9e6524
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-08-13 12:51:09 +0100

    Added responsive CSS to NavBar

--- src/Containers/NavBar/NavBar.js
+++ src/Containers/NavBar/NavBar.js
@@ -5,16 +5,16 @@ import Logo from '../../Assets/SVG/Logo.svg'
 const NavBar = () => {
     return (
         <header>
-            <nav className = {style.NavBar}>
-                <img src={Logo} alt="" className = {style.NavLogo}/>
-                    {/* Logo will be here */}
+            <nav className={style.NavBar}>
+                <img src={Logo} alt="Main Logo" className={style.NavLogo} />
+                {/* Logo will be here */}
 
-                <div className = {style.NavItems}>
+                <div className={style.NavItems}>
                     <ul>
                         <li>Home</li>
                         <li>Portfolio</li>
                         <li>Contact</li>
-                        <li className = {style.Resume}>Resume</li>
+                        <li className={style.Resume}>Resume</li>
                     </ul>
                 </div>
             </nav>
@@ -23,4 +23,8 @@ const NavBar = () => {
     )
 }
 
+
+// <div className={style.NavItemsContainer}>
+//                         
+//                     </div>
 export default NavBar;
